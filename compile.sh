377073a1bb88c6b169025dadb6149c1c8127d12a
Jens Domke <domke.j.aa@m.titech.ac.jp>
2018-02-13 13:13:55 +0900

    added missing refiner lib to ffb

--- compile.sh
+++ compile.sh
@@ -281,8 +281,17 @@ if [ ! -f $ROOTDIR/FFB/bin/les3x.mpi ]; then
 		make install
 		cd $ROOTDIR/FFB/src
 	fi
+	if [ ! -f ./REVOCAP_Refiner-1.1.01/lib/x86_64-linux-intel/libRcapRefiner.a ]; then
+		tar xzf $ROOTDIR/dep/REVOCAP_Refiner-1.1.01.tgz
+		cd ./REVOCAP_Refiner-1.1.01
+		rm ./MakefileConfig.in; ln -s ./MakefileConfig.LinuxIntelCompiler ./MakefileConfig.in
+		sed -i '/#include <sstream>/a #include <stdlib.h>' ./RevocapIO/kmbHecmwIO_V3.cpp
+		make
+		cd $ROOTDIR/FFB/src
+	fi
 	cp ./make_setting.intel ./make_setting
 	sed -i -e 's/^DEFINE += -DNO_METIS/#DEFINE += -DNO_METIS/g' -e "s#\$(HOME)/opt_intel/metis5#$ROOTDIR/FFB/src/metis-5.1.0#g" ./make_setting
+	sed -i -e 's/^DEFINE += -DNO_REFINER/#DEFINE += -DNO_REFINER/g' -e "s#\$(HOME)/opt_intel/REVOCAP_Refiner#$ROOTDIR/FFB/src/REVOCAP_Refiner-1.1.01#g" -e "s#REFINER)/lib #REFINER)/lib/x86_64-linux-intel #" ./make_setting
 	make
 	cd $ROOTDIR
 fi
