1b65ad7b9511f8ca887de55951419ccfc654e09a
Jens Domke <domke.j.aa@m.titech.ac.jp>
2018-02-15 13:32:53 +0900

    found best mVMC conf

--- conf/mvmc.sh
+++ conf/mvmc.sh
@@ -7,7 +7,7 @@ export INPUT="./multiDir.def"
 if [ "x`lscpu | grep '^Model name.*E5-2650' | wc -l`" = "x1" ]; then
 	# on "normal" Xeon
 	export TESTCONF="1|96 1|48 1|32 1|24 1|12 1|6 2|24 2|12 2|6 4|12 4|6 6|8 6|4 12|4 12|2 24|1 24|2 32|1 32|2 48|1 48|2"
-	export BESTCONF=""
+	export BESTCONF="4|6"
 else
 	# on one of the Phi
 	export TESTCONF=""
---
+++ run/mvmc/best.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
+cd $ROOTDIR
+
+source /opt/intel/parallel_studio_xe_2018.1.038/bin/psxevars.sh intel64 > /dev/null 2>&1
+ulimit -s unlimited
+ulimit -n 4096
+MPIEXECOPT="-host `hostname`"
+
+# ============================ mVMC ===========================================
+source conf/mvmc.sh
+NumRUNS=10
+LOG="$ROOTDIR/log/bestrun/mvmc.log"
+mkdir -p `dirname $LOG`
+cd $APPDIR
+for BEST in $BESTCONF; do
+	NumMPI="`echo $BEST | cut -d '|' -f1`"
+	NumOMP="`echo $BEST | cut -d '|' -f2`"
+	echo "mpiexec $MPIEXECOPT -genv OMP_NUM_THREADS=$NumOMP -n $NumMPI $BINARY $INPUT" >> $LOG 2>&1
+	for i in `seq 1 $NumRUNS`; do
+		mpiexec $MPIEXECOPT -genv OMP_NUM_THREADS=$NumOMP -n $NumMPI $BINARY $INPUT >> $LOG 2>&1
+		cat Lx10Ly10_J1.0/zvo_HitachiTimer.dat >> $LOG 2>&1
+		rm -f Lx10Ly10_J1.0/zvo_*
+	done
+done
+echo "Best mVMC run:"
+BEST="`grep '^VMCParaOpt' $LOG | awk -F ']' '{print $2}' | sort -g | head -1`"
+grep "$BEST\|mpiexec" $LOG | grep -B1 "$BEST"
+echo ""
+cd $ROOTDIR
