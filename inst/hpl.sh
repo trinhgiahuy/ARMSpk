dc7a133dbab55c7ff8aa69354c81e05d72bbc89c
Jens Domke <domke.j.aa@m.titech.ac.jp>
2018-03-01 17:49:25 +0900

    little ugly dot

--- inst/hpl.sh
+++ inst/hpl.sh
@@ -16,7 +16,7 @@ if [ ! -f $ROOTDIR/$BM/bin/Linux_Intel64/xhpl ]; then
 	mkdir -p $ROOTDIR/$BM/
 	cd $ROOTDIR/$BM/
 	wget http://www.netlib.org/benchmark/hpl/hpl-2.2.tar.gz
-	tar xzf ./hpl-2.2.tar.gz . -C $ROOTDIR/$BM --strip-components 1
+	tar xzf ./hpl-2.2.tar.gz -C $ROOTDIR/$BM --strip-components 1
 	patch -p1 < $ROOTDIR/patches/*1-${BM}*.patch
 	sed -i -e "s#\$(HOME)/hpl#$ROOTDIR/$BM#g" ./Make.Linux_Intel64
 	make arch=Linux_Intel64
