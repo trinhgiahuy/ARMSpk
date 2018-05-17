7ac497f23c7c3998fbd4bc2936b045711fbd22b5
Jens Domke <domke.j.aa@m.titech.ac.jp>
2018-05-17 20:08:21 +0900

    minor update

--- inst/swfft.sh
+++ inst/swfft.sh
@@ -15,7 +15,7 @@ alias ld=`which xild`
 
 BM="SWFFT"  # fortran version is 5-10% faster in my tests
 VERSION="d0ef31454577740fbb87618cc35789b7ef838238"
-if [ ! -f $ROOTDIR/$BM/build.xeon/TestDfft ]; then
+if [ ! -f $ROOTDIR/$BM/build.openmp/TestDfft ]; then
 	cd $ROOTDIR/$BM/
 	git checkout -b precision ${VERSION}
 	git apply --check $ROOTDIR/patches/*1-${BM}*.patch
@@ -24,24 +24,14 @@ if [ ! -f $ROOTDIR/$BM/build.xeon/TestDfft ]; then
 		wget http://fftw.org/fftw-3.3.4.tar.gz
 		tar xzf fftw-3.3.4.tar.gz
 		cd ./fftw-3.3.4/
-		./configure --prefix=`pwd`/../fftw-xmic --disable-mpi --enable-openmp --disable-fortran --enable-sse2 --enable-avx CC=icc
-		make -j CFLAGS="-O3 -ipo -xHost -xMIC-AVX512 -fp-model fast=2 -no-prec-div -qoverride-limits"
-		make install
-		make distclean
-		./configure --prefix=`pwd`/../fftw-xeon --disable-mpi --enable-openmp --disable-fortran --enable-sse2 --enable-avx CC=icc
-		make -j CFLAGS="-O3 -ipo -xHost -xCORE-AVX2 -fp-model fast=2 -no-prec-div -qoverride-limits"
+		./configure --prefix=`pwd`/../fftw --disable-mpi --enable-openmp --disable-fortran --enable-sse2 --enable-avx CC=icc
+		make -j CFLAGS="-O3 -ipo -xHost -fp-model fast=2 -no-prec-div -qoverride-limits"
 		make install
 		cd $ROOTDIR/$BM/
 	fi
 	oldPATH=$PATH
-	export PATH=$oldPATH:`pwd`/fftw-xeon/bin
+	export PATH=$oldPATH:`pwd`/fftw/bin
 	make -f GNUmakefile.openmp
-	mv build.openmp build.xeon
-	make -f GNUmakefile.openmp clean
-	export PATH=$oldPATH:`pwd`/fftw-xmic/bin
-	make -f GNUmakefile.openmp
-	mv build.openmp build.xmic
-	make -f GNUmakefile.openmp clean
 	export PATH=$oldPATH
 	cd $ROOTDIR
 fi
