bd1f6b7a7f1acdcd81bf3f76dbae9fee0b3ca4ac
Jens Domke <domke.j.aa@m.titech.ac.jp>
2018-07-31 23:13:42 +0900

    maybe this fixed the vtune problem

--- inst/_init.sh
+++ inst/_init.sh
@@ -18,10 +18,11 @@ sleep 10
 
 echo -e '\nInstalling known dependencies'
 if [[ -f /etc/redhat-release ]];then
-	sudo yum -y install cmake autoconf automake libtool cpupowerutils gcc libstdc++ gcc-c++ gcc-gfortran bzip2 patch zlib-devel ncurses-devel kernel-devel bc gawk coreutils grep
-	# vim screen gtk2 gtk3 pango xorg-x11-server-Xorg
+	sudo yum -y install cmake autoconf automake libtool cpupowerutils gcc libstdc++ gcc-c++ gcc-gfortran bzip2 patch zlib-devel ncurses-devel kernel-devel bc gawk coreutils grep perf
+	# vim screen
+	# for Intel Parallel XE: gtk2 gtk3 pango xorg-x11-server-Xorg
 else
-	echo -e '\nNote: untested linux distro; please install -- cmake autoconf automake libtool cpupowerutils gcc libstdc++ gcc-c++ gcc-gfortran bzip2 patch zlib-devel ncurses-devel kernel-devel bc gawk coreutils grep -- yourself;\n      everything hereafter may break, no guarantees ;-)'
+	echo -e '\nNote: untested linux distro; please install -- cmake autoconf automake libtool cpupowerutils gcc libstdc++ gcc-c++ gcc-gfortran bzip2 patch zlib-devel ncurses-devel kernel-devel bc gawk coreutils grep perf -- yourself;\n      everything hereafter may break, no guarantees ;-)'
 	sleep 10
 fi
 
@@ -34,10 +35,13 @@ echo -e '\nDisabling NMI watchdog'
 sudo sh -c "echo 0 > /proc/sys/kernel/nmi_watchdog"
 sudo sh -c "echo 'kernel.nmi_watchdog=0' >> /etc/sysctl.conf"
 
-echo -e '\nCorrectly adding Intel VTune Sampling Drivers'
+echo -e '\nRemoving broken Intel VTune Sampling Drivers and use perf'
 cd ${VTUNE_AMPLIFIER_2018_DIR}/sepdk/src
-sudo ./insmod-sep -r -g `whoami` -pu
-sudo ./boot-script --install -g `whoami` -pu
+sudo ./rmmod-sep -R
+sudo ./boot-script --uninstall
+sudo sh -c 'echo 0 > /proc/sys/kernel/perf_event_paranoid'
+#sudo ./insmod-sep -r -g `whoami` -pu
+#sudo ./boot-script --install -g `whoami` -pu
 
 echo -e '\nInstalling Likwid'
 BM="likwid"
@@ -82,11 +86,11 @@ fi
 echo -e '\nInstalling MSR/MSR-Safe'
 BM="msr-safe"
 VERSION="b5bdf8b200db5a0bfa7e9ba2aadb85159f72c697"
+WL="`printf 'wl_%.2x%x\n' $(lscpu | awk '/CPU family:/{print $3}') $(lscpu | awk '/Model:/{print $2}')`"
 if [ ! -f $ROOTDIR/dep/$BM/msr-safe.ko ] || [ ! -r /dev/cpu/0/msr ]; then
 	cd $ROOTDIR/dep/$BM/
 	git checkout -b precision ${VERSION}
 	make
-	WL="`printf 'wl_%.2x%x\n' $(lscpu | awk '/CPU family:/{print $3}') $(lscpu | awk '/Model:/{print $2}')`"
 	# use KNL whitelist for KNM nodes
 	if [ ! -e ./whitelists/wl_0685 ]; then cp ./whitelists/wl_0657 ./whitelists/wl_0685; fi
 	sudo insmod msr-safe.ko
@@ -100,4 +104,18 @@ if [ ! -f $ROOTDIR/dep/$BM/msr-safe.ko ] || [ ! -r /dev/cpu/0/msr ]; then
 	sudo chmod go+rw /dev/mem
 	cd $ROOTDIR
 fi
+
+echo -e "\nIn case of reboot, run again:
+sudo sh -c 'echo 0 > /proc/sys/kernel/perf_event_paranoid'
+sudo sh -c 'echo 0 > /proc/sys/kernel/nmi_watchdog'
+cd $ROOTDIR/dep/msr-safe
+sudo insmod msr-safe.ko
+sudo chmod go+rw /dev/cpu/msr_whitelist
+sudo cat ./whitelists/${WL} > /dev/cpu/msr_whitelist
+sudo touch /sys/firmware/acpi/tables/MCFG
+sudo chmod go+rw /sys/firmware/acpi/tables/MCFG
+sudo chmod go+rw /dev/cpu/*/msr
+sudo chmod go+rw /dev/cpu/*/msr_safe
+sudo chmod go+rw /proc/bus/pci/*/*.*
+sudo chmod go+rw /dev/mem"
 
