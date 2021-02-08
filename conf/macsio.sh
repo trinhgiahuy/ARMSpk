c8b0e507a5ca13de9bfd9458b58795834f79a873
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-02-08 22:53:28 +0900

    didnt dump if num dumps <1

--- conf/macsio.sh
+++ conf/macsio.sh
@@ -2,8 +2,9 @@
 
 export APPDIR="./MACSio"
 export BINARY="./macsio/macsio"
-export MAXNDPP=$((2*2*2*2*2*3*3))
-export INPUT="--units_prefix_system decimal --num_dumps NDPP"
+export MAXNDPP=1024  #increase by ~3x to support 1k MPI threads; $((2*2*2*2*2*3*3))
+# but decrease size of dump by ~3x from 80k to 20k
+export INPUT="--part_size 20000 --units_prefix_system decimal --num_dumps NDPP"
 export NumRunsTEST=3
 export NumRunsBEST=10
 export MAXTIME="2m"
@@ -15,7 +16,7 @@ if [[ $HOSTNAME = *"${XEONHOST}"* ]]; then
 	# on "normal" Xeon
 	export TESTCONF="1|1 4|1 6|1 12|1 24|1 32|1 48|1 96|1"
 	export BESTCONF="4|1"
-	export BESTCONF="256|1 1024|1"
+	export SCALCONF="256|1 1024|1"
 elif [[ $HOSTNAME = *"${IKNLHOST}"* ]]; then
 	# on one of the Phi (knl)
 	export TESTCONF="16|1 32|1 64|1 96|1 128|1 192|1 256|1"
