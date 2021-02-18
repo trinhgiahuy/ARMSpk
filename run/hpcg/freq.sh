d6c6ec06910a65356568f002457f5558255ca929
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-02-18 21:42:32 +0900

    fucking hell

--- run/hpcg/freq.sh
+++ run/hpcg/freq.sh
@@ -32,15 +32,21 @@ for FREQ in $FREQR; do
 		NumOMP="`echo $BEST | cut -d '|' -f2`"
 		# test to identify hpcg's internal dimensions
 		rm -f hpcg_log_* n*.yaml
+		rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 		mpiexec $MPIEXECOPT -n $NumMPI $BINARY -n 1 > /dev/null 2>&1
-		if [ ! "x$?" = "x0" ] || [ ! -f n*.yaml ]; then continue; fi
-		X="`grep 'npx:' n*.yaml | awk -F 'npx:' '{print $2}'`"
-		Y="`grep 'npy:' n*.yaml | awk -F 'npy:' '{print $2}'`"
-		Z="`grep 'npz:' n*.yaml | awk -F 'npz:' '{print $2}'`"
+		if [ ! "x$?" = "x0" ]; then continue; fi
+		if [ -f n*.yaml ]; then
+			X=$(($MAXXYZ / `grep 'npx:' n*.yaml | awk -F 'npx:' '{print $2}'`))
+			Y=$(($MAXXYZ / `grep 'npy:' n*.yaml | awk -F 'npy:' '{print $2}'`))
+			Z=$(($MAXXYZ / `grep 'npz:' n*.yaml | awk -F 'npz:' '{print $2}'`))
+		elif [ -f HPCG-Benchmark_3*.txt ]; then
+			#non-intel version needs to be div8 https://github.com/hpcg-benchmark/hpcg/issues/47
+			X=$((($MAXXYZ / `grep 'npx=' HPCG-Benchmark_3*.txt | awk -F 'npx=' '{print $2}'` / 8) * 8))
+			Y=$((($MAXXYZ / `grep 'npy=' HPCG-Benchmark_3*.txt | awk -F 'npy=' '{print $2}'` / 8) * 8))
+			Z=$((($MAXXYZ / `grep 'npz=' HPCG-Benchmark_3*.txt | awk -F 'npz=' '{print $2}'` / 8) * 8))
+		else continue; fi
 		rm -f hpcg_log_* n*.yaml
-		X=$(($MAXXYZ / $X))
-		Y=$(($MAXXYZ / $Y))
-		Z=$(($MAXXYZ / $Z))
+		rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 		INPUT="`echo $DEFINPUT | sed -e \"s/NX/$X/\" -e \"s/NY/$Y/\" -e \"s/NZ/$Z/\"`"
 		echo "mpiexec $MPIEXECOPT -genv OMP_NUM_THREADS=$NumOMP -n $NumMPI $BINARY $INPUT" >> $LOG 2>&1
 		for i in `seq 1 $NumRunsBEST`; do
@@ -51,6 +57,9 @@ for FREQ in $FREQR; do
 			cat hpcg_log_* >> $LOG 2>&1
 			cat n*.yaml >> $LOG 2>&1
 			rm -f hpcg_log_* n*.yaml
+			cat hpcg20*.txt >> $LOG 2>&1
+			cat HPCG-Benchmark_3*.txt >> $LOG 2>&1
+			rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 			echo "Total running time: `echo \"$ENDED - $START\" | bc -l`" >> $LOG 2>&1
 		done
 	done
--- run/hpcg/prof.sh
+++ run/hpcg/prof.sh
@@ -49,15 +49,21 @@ for BEST in $BESTCONF $SCALCONF; do
 	ProcElem=$(((NumCORES / NumMPI) + (NumCORES < NumMPI)))
 	# test to identify hpcg's internal dimensions
 	rm -f hpcg_log_* n*.yaml
+	rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 	mpiexec $MPIEXECOPT -n $NumMPI $BINARY -n 1 > /dev/null 2>&1
-	if [ ! "x$?" = "x0" ] || [ ! -f n*.yaml ]; then continue; fi
-	X="`grep 'npx:' n*.yaml | awk -F 'npx:' '{print $2}'`"
-	Y="`grep 'npy:' n*.yaml | awk -F 'npy:' '{print $2}'`"
-	Z="`grep 'npz:' n*.yaml | awk -F 'npz:' '{print $2}'`"
+	if [ ! "x$?" = "x0" ]; then continue; fi
+	if [ -f n*.yaml ]; then
+		X=$(($MAXXYZ / `grep 'npx:' n*.yaml | awk -F 'npx:' '{print $2}'`))
+		Y=$(($MAXXYZ / `grep 'npy:' n*.yaml | awk -F 'npy:' '{print $2}'`))
+		Z=$(($MAXXYZ / `grep 'npz:' n*.yaml | awk -F 'npz:' '{print $2}'`))
+	elif [ -f HPCG-Benchmark_3*.txt ]; then
+		#non-intel version needs to be div8 https://github.com/hpcg-benchmark/hpcg/issues/47
+		X=$((($MAXXYZ / `grep 'npx=' HPCG-Benchmark_3*.txt | awk -F 'npx=' '{print $2}'` / 8) * 8))
+		Y=$((($MAXXYZ / `grep 'npy=' HPCG-Benchmark_3*.txt | awk -F 'npy=' '{print $2}'` / 8) * 8))
+		Z=$((($MAXXYZ / `grep 'npz=' HPCG-Benchmark_3*.txt | awk -F 'npz=' '{print $2}'` / 8) * 8))
+	else continue; fi
 	rm -f hpcg_log_* n*.yaml
-	X=$(($MAXXYZ / $X))
-	Y=$(($MAXXYZ / $Y))
-	Z=$(($MAXXYZ / $Z))
+	rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 	INPUT="`echo $DEFINPUT | sed -e \"s/NX/$X/\" -e \"s/NY/$Y/\" -e \"s/NZ/$Z/\"`"
 	if [ "x$RUNSDE" = "xyes" ]; then
 		echo "=== sde run ===" >> $LOG 2>&1
@@ -67,6 +73,9 @@ for BEST in $BESTCONF $SCALCONF; do
 			cat hpcg_log_* >> $LOG 2>&1
 			cat n*.yaml >> $LOG 2>&1
 			rm -f hpcg_log_* n*.yaml
+			cat hpcg20*.txt >> $LOG 2>&1
+			cat HPCG-Benchmark_3*.txt >> $LOG 2>&1
+			rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 		done
 		mkdir -p ${LOG}_${NumMPI}_${NumOMP}_sde; mv dcfg-out.* ${LOG}_${NumMPI}_${NumOMP}_sde/
 	fi
@@ -79,6 +88,7 @@ for BEST in $BESTCONF $SCALCONF; do
 			echo "$PCM mpiexec $MPIEXECOPT --map-by slot:pe=$ProcElem -x OMP_NUM_THREADS=$NumOMP -n $NumMPI $BINARY $INPUT" >> $LOG 2>&1
 			$PCM mpiexec $MPIEXECOPT --map-by slot:pe=$ProcElem -x OMP_NUM_THREADS=$NumOMP -n $NumMPI $BINARY $INPUT >> $LOG 2>&1
 			rm -f hpcg_log_* n*.yaml
+			rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 		done
 	fi
 	if [ "x$RUNVTUNE" = "xyes" ]; then
@@ -89,6 +99,7 @@ for BEST in $BESTCONF $SCALCONF; do
 			amplxe-cl -report summary -q -result-dir ./oVTP.`hostname` >> $LOG 2>&1
 			rm -rf ./oVTP.`hostname`
 			rm -f hpcg_log_* n*.yaml
+			rm -f hpcg20*.txt HPCG-Benchmark_3*.txt
 		done
 	fi
 done
