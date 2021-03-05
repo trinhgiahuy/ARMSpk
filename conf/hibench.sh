ec708d3e76cda575b89876a31021959dc546e13d
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-03-05 18:09:48 +0900

    improving hibench a bit

--- conf/hibench.sh
+++ conf/hibench.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
 export APPDIR="./HiBench"
-export BINARY="./bin/run_all.sh"
+#export BINARY="./bin/run_all.sh"
+export BINARYS="bin/workloads/graph/nweight/spark/run.sh
+bin/workloads/micro/repartition/spark/run.sh
+bin/workloads/micro/sleep/spark/run.sh
+bin/workloads/micro/sort/spark/run.sh
+bin/workloads/micro/terasort/spark/run.sh
+bin/workloads/micro/wordcount/spark/run.sh
+bin/workloads/ml/als/spark/run.sh
+bin/workloads/ml/bayes/spark/run.sh
+bin/workloads/ml/gbt/spark/run.sh
+bin/workloads/ml/kmeans/spark/run.sh
+bin/workloads/ml/lda/spark/run.sh
+bin/workloads/ml/linear/spark/run.sh
+bin/workloads/ml/lr/spark/run.sh
+bin/workloads/ml/pca/spark/run.sh
+bin/workloads/ml/rf/spark/run.sh
+bin/workloads/ml/svd/spark/run.sh
+bin/workloads/ml/svm/spark/run.sh
+bin/workloads/sql/aggregation/spark/run.sh
+bin/workloads/sql/join/spark/run.sh
+bin/workloads/sql/scan/spark/run.sh
+bin/workloads/websearch/pagerank/spark/run.sh"
 export INPUT=""
 export NumRunsTEST=3
 export NumRunsBEST=10
-export MAXTIME="90m"
+export MAXTIME="10m"
 export RUNSDE="yes"
 export RUNPCM="no"
 export RUNVTUNE="no"
