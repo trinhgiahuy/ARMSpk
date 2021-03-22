135478149869ed8e9e401a8d13a56b8d6dd21f4e
Jens Domke <673751-domke@users.noreply.gitlab.com>
2021-03-22 20:43:21 +0900

    fcc-ing miniamr and modylas but macsio is broken

--- inst/miniamr.sh
+++ inst/miniamr.sh
@@ -21,7 +21,7 @@ if [ -z $1 ]; then
 	export OMPI_CXX=$I_MPI_CXX
 	export OMPI_F77=$I_MPI_F77
 	export OMPI_FC=$I_MPI_F90
-else
+elif [[ "$1" = *"gnu"* ]]; then
 	source $ROOTDIR/dep/spack/share/spack/setup-env.sh
 	spack load gcc@8.4.0
 	spack load openmpi@3.1.6%gcc@8.4.0
@@ -29,6 +29,12 @@ else
 	export OMPI_CXX=g++
 	export OMPI_F77=gfortran
 	export OMPI_FC=gfortran
+elif [[ "$1" = *"fuji"* ]]; then
+	module load FujitsuCompiler/202007
+	export LD_LIBRARY_PATH=$ROOTDIR/dep/mpistub/lib:$LD_LIBRARY_PATH
+else
+	echo 'wrong compiler'
+	exit 1
 fi
 
 BM="MiniAMR"
@@ -43,9 +49,12 @@ if [ ! -f $ROOTDIR/$BM/ref/ma.x ]; then
 		for x in `/bin/grep -r 'exchange(' . | cut -d':' -f1 | sort -u`; do sed -i -e 's/exchange(/xxxexchange(/' $x; done
 		if [ -z $1 ]; then
 			sed -i -e 's/-L${ADVISOR/-static -static-intel -qopenmp-link=static -L${ADVISOR/' ./Makefile
-		else
+		elif [[ "$1" = *"gnu"* ]]; then
 			sed -i -e 's/-ipo -xHost/-march=native/g' -e 's/-qopenmp/-fopenmp/g' -e 's# -I${ADVISOR_2018_DIR}/include##g' -e 's# -L${ADVISOR_2018_DIR}/lib64 -littnotify# -static#g' ./Makefile
 			for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' $FILE; done
+		elif [[ "$1" = *"fuji"* ]]; then
+			sed -i -e 's/^CC .*=.*/CC = fccpx/' -e 's/^LD .*=.*/LD = fccpx/' -e 's/ -lm / /g' -e 's/-ipo -xHost/-march=native/g' -e 's/-qopenmp/-fopenmp/g' -e "s# -I\${ADVISOR_2018_DIR}/include# -I$ROOTDIR/dep/mpistub/include/mpistub#g" -e "s# -L\${ADVISOR_2018_DIR}/lib64 -littnotify# -Wl,-rpath -Wl,$ROOTDIR/dep/mpistub/lib/mpistub -L$ROOTDIR/dep/mpistub/lib/mpistub -lmpi -Bstatic -lm#g" ./Makefile
+			for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#include <time.h>\n#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' -e '/double mkrts, mkrte;/i struct timespec mkrtsclock;' -e 's/mkrts = MPI_Wtime();/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrts = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/' -e 's/mkrte = MPI_Wtime();/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrte = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/' $FILE; done
 		fi
 		make
 	done
--- inst/modylas.sh
+++ inst/modylas.sh
@@ -21,7 +21,7 @@ if [ -z $1 ]; then
 	export OMPI_CXX=$I_MPI_CXX
 	export OMPI_F77=$I_MPI_F77
 	export OMPI_FC=$I_MPI_F90
-else
+elif [[ "$1" = *"gnu"* ]]; then
 	source $ROOTDIR/dep/spack/share/spack/setup-env.sh
 	spack load gcc@8.4.0
 	spack load openmpi@3.1.6%gcc@8.4.0
@@ -29,6 +29,12 @@ else
 	export OMPI_CXX=g++
 	export OMPI_F77=gfortran
 	export OMPI_FC=gfortran
+elif [[ "$1" = *"fuji"* ]]; then
+	module load FujitsuCompiler/202007
+	export LD_LIBRARY_PATH=$ROOTDIR/dep/mpistub/lib:$LD_LIBRARY_PATH
+else
+	echo 'wrong compiler'
+	exit 1
 fi
 
 if [ ! -f $ROOTDIR/dep/modylas-mini-1.0.0.tar.gz ]; then
@@ -47,10 +53,15 @@ if [ ! -f $ROOTDIR/$BM/src/modylas_mini ]; then
 	if [ -z $1 ]; then
 		rm make_setting; ln -s make_setting.intel make_setting
 		sed -i -e 's/-L${ADVISOR/-static -static-intel -qopenmp-link=static -L${ADVISOR/' ./make_setting
-	else
+	elif [[ "$1" = *"gnu"* ]]; then
 		rm make_setting; ln -s make_setting.gcc make_setting
 		sed -i -e 's/-O3/-O3 -march=native\nLIBS += -static/g' ./make_setting
 		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' $FILE; done
+	elif [[ "$1" = *"fuji"* ]]; then
+		rm make_setting; ln -s make_setting.gcc make_setting
+		sed -i -e 's/^FC =.*/FC = frtpx/g' -e "s#^CC =.*#CC = fccpx -I$ROOTDIR/dep/mpistub/include/mpistub#g" -e "s#-O3#-O3 -march=native -I$ROOTDIR/dep/mpistub/include/mpistub\nLIBS += -Wl,-rpath -Wl,$ROOTDIR/dep/mpistub/lib/mpistub -L$ROOTDIR/dep/mpistub/lib/mpistub -lmpi -lmpifort -Bstatic#g" ./make_setting
+		sed -i -e '/use mpi/d' -e "/implicit none/a \  include 'mpif.h'" ./ma_prof/src/mod_maprof.F90
+		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#include <time.h>\n#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' -e '/double mkrts, mkrte;/i struct timespec mkrtsclock;' -e 's/mkrts = MPI_Wtime();/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrts = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/' -e 's/mkrte = MPI_Wtime();/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrte = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/' $FILE; done
 	fi
 	make
 	cd $ROOTDIR
