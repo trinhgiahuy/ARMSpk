708c90fa643d0abe0e73b2d3f4c58d00dd67583e
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-03-30 22:22:41 +0900

    run gem on other nodes too

--- run/xsbench/gem5.sh
+++ run/xsbench/gem5.sh
@@ -7,7 +7,7 @@ source $ROOTDIR/conf/host.cfg
 ulimit -s unlimited
 ulimit -n 8192; ulimit -u 8192
 
-XEONHOST=kiev	# we don't care where gem runs
+export XEONHOST="kiev" # we don't care where gem runs
 GEM5="$ROOTDIR/dep/gem5_riken/build/ARM/gem5.opt"
 GEM5SE="$ROOTDIR/dep/gem5_riken/configs/example/se.py"
 
