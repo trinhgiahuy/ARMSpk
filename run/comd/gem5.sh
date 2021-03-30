bf9073860aebb5c9850a0af124a228528cc8fdb9
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-03-30 23:56:06 +0900

    upsi nr2

--- run/comd/gem5.sh
+++ run/comd/gem5.sh
@@ -30,9 +30,10 @@ for BEST in $BESTCONF; do
 	NumMPI=1
 	if [ $1 -eq 1 ]; then NumOMP="20"; elif [ $1 -eq 2 ]; then NumOMP="32"; fi
 	echo -e "OMP_NUM_THREADS=$NumOMP\nOMP_NUM_PARALELL=$NumOMP\nFLIB_FASTOMP=FALSE\nFLIB_CNTL_BARRIER_ERR=FALSE" > ./omp${NumOMP}.txt
-	X="`echo $BEST | cut -d '|' -f3`"
-	Y="`echo $BEST | cut -d '|' -f4`"
-	Z="`echo $BEST | cut -d '|' -f5`"
+	X=1; Y=1; Z=1;  #XXX: need to change this if we set NumMPI=1
+	#X="`echo $BEST | cut -d '|' -f3`"
+	#Y="`echo $BEST | cut -d '|' -f4`"
+	#Z="`echo $BEST | cut -d '|' -f5`"
 	INPUT="`echo $DEFINPUT | sed -e \"s/PX/$X/\" -e \"s/PY/$Y/\" -e \"s/PZ/$Z/\"`"
 	echo "=== gem5 run ===" >> $LOG 2>&1
 	echo "$PIN $GEM5 -d `dirname $LOG` $GEM5SE -c $BINARY -o \"$INPUT\" -n $NumOMP -e ./omp${NumOMP}.txt $ARCHCONF" >> $LOG 2>&1
