5e5a5e857e6877698ba6ce5427f93e4ff4061637
Jens Domke <673751-domke@users.noreply.gitlab.com>
2021-03-30 21:26:50 +0900

    xsbenh running thru gem5

---
+++ run/xsbench/gem5.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../"
+cd $ROOTDIR
+
+source $ROOTDIR/conf/host.cfg
+ulimit -s unlimited
+ulimit -n 8192; ulimit -u 8192
+
+GEM5="$ROOTDIR/dep/gem5_riken/build/ARM/gem5.opt $ROOTDIR/dep/gem5_riken/configs/example/se.py"
+
+if [ $1 -eq 1 ]; then
+	ARCHCONF="--cpu-type=O3_ARM_PostK_3 --caches --l2_size=16MB --mem_bus_width=64 --mem_resp_width=128 --mem-size=32GB"	# traditional
+elif [ $1 -eq 2 ]; then
+	# aggressive
+	ARCHCONF="--cpu-type=O3_ARM_PostK_3 --caches --l2_size=256MB --mem_bus_width=96 --mem_resp_width=192 --mem-size=32GB"	# aggressive
+else echo "err: missing which arch conf to run"; exit 1; fi
+
+# special core pinning?
+if [ -n $2 ] && which numactl >/dev/null 2>&1; then PIN="numactl -C $2"; else PIN=""; fi
+
+# ============================ XSBench ========================================
+source conf/xsbench.sh
+LOG="$ROOTDIR/log/`hostname -s`/gem5run/xsbench.log"
+mkdir -p `dirname $LOG`
+cd $APPDIR
+DEFINPUT=$INPUT
+for BEST in 1; do
+	NumMPI=1
+	if [ $1 -eq 1 ]; then NumOMP="20"; elif [ $1 -eq 2 ]; then NumOMP="32"; fi
+	echo -e "OMP_NUM_THREADS=$NumOMP\nOMP_NUM_PARALELL=$NumOMP\nFLIB_FASTOMP=FALSE\nFLIB_CNTL_BARRIER_ERR=FALSE" > ./omp${NumOMP}.txt
+	INPUT="`echo $DEFINPUT | sed -e \"s/OMPNT/$NumOMP/\"`"
+	echo "=== gem5 run ===" >> $LOG 2>&1
+	echo "$PIN $GEM5 -c $BINARY -o \"$INPUT\" -n $NumOMP -e ./omp${NumOMP}.txt $ARCHCONF\"" >> $LOG 2>&1
+	$PIN $GEM5 -c $BINARY -o "$INPUT" -n $NumOMP -e ./omp${NumOMP}.txt $ARCHCONF >> $LOG 2>&1
+done
+cd $ROOTDIR
