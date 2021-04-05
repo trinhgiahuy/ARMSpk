cb6d98bb42dcc837144c410d7c7bcaac62dd99ad
Jens Domke <jens.domke@riken.jp>
2021-04-05 23:56:00 +0900

    ported spec omp to fu DRAFT

--- inst/spec_omp.sh
+++ inst/spec_omp.sh
@@ -30,9 +30,14 @@ submit         = ulimit -n 4096; ulimit -s unlimited; sde64 -sse-sde -disasm_att
 BOPTS          = -O3 -qopenmp -xHOST -no-prec_div -fp-model fast=2 -fma
 BLINK          = -static -static-intel -qopenmp-link=static
 %elif '%{COMP}' eq 'fuji'
+%if '%{HOST}' eq 'peach'
 submit         = ulimit -n 4096; ulimit -s unlimited; bash %{RESDIR}/gem5wrap.sh \$command
 BOPTS          = -Kfast,eval_concurrent,openmp -O3 -march=armv8.3-a+sve -funroll-loops
 BLINK          = -Bstatic -lm
+%else
+submit         = ulimit -s unlimited; \$command
+BOPTS          = -Nclang -Kfast,eval_concurrent,openmp -O3 -march=armv8.3-a+sve -funroll-loops
+%endif
 %else
 %error wrong or unsupported COMP variable specified
 %endif
@@ -62,9 +67,15 @@ CC             = icc
 CXX            = icpc
 %elif '%{COMP}' eq 'fuji'
 sw_compiler    = FUJITSU Software Technical Computing Suite
+%if '%{HOST}' eq 'peach'
 FC             = frtpx
 CC             = fccpx
 CXX            = FCCpx
+%else
+FC             = frt
+CC             = fcc
+CXX            = FCC
+%endif
 %endif
 FOPTIMIZE      = \${BOPTS}
 COPTIMIZE      = \${BOPTS}
@@ -85,6 +96,8 @@ FPORTABILITY   = -free
 FPORTABILITY   = -mcmodel=medium
 LDOPT          = -shared-intel
 PASS1_LDOPT    = -shared-intel
+%elif '%{COMP}' eq 'fuji'
+FPORTABILITY   = -mcmodel=large
 %endif
 
 359.botsspar=default=default=default:
@@ -97,6 +110,8 @@ strict_rundir_verify = 0
 FPORTABILITY   = -mcmodel=medium
 LDOPT          = -shared-intel
 PASS1_LDOPT    = -shared-intel
+%elif '%{COMP}' eq 'fuji'
+FPORTABILITY   = -mcmodel=large
 %endif
 
 367.imagick=default=default=default:
@@ -126,39 +141,57 @@ if [ -z $1 ]; then
 elif [[ "$1" = *"gnu"* ]]; then
 	source $ROOTDIR/dep/spack/share/spack/setup-env.sh
 	spack load gcc@8.4.0
+elif [[ "`hostname -s`" = *"fn01"* ]] && [[ "$1" = *"fuji"* ]]; then
+	echo "does not compile on login node"; exit 1
 elif [[ "$1" = *"fuji"* ]]; then
-	if [[ "`hostname -s`" = *"peach"* ]]; then
-		module load FujitsuCompiler/202007
-		#alias fcc=fccpx
-		#alias FCC=FCCpx
-		#alias frt=frtpx	# XXX: not working
-	else
-		echo "check fugaku later"; exit 1
-	fi
+	if [[ "`hostname -s`" = *"peach"* ]]; then module load FujitsuCompiler/202007; fi
 else
 	echo 'wrong compiler'
 	exit 1
 fi
 
 BM="SPEC_OMP"
-dump_omp_config $ROOTDIR/$BM/config/nedo.cfg; exit
 if [ ! -f $ROOTDIR/$BM/bin/runcpu ]; then
-	if [ ! -f $ROOTDIR/dep/omp2012-1.1.iso ]; then
-		echo -e "ERR: cannot find omp2012-1.1.iso under dep/; please fix it!"
-		exit 1
-	fi
-	cd $ROOTDIR/dep/
-	mkdir -p /tmp/mnt_$BM
-	fuseiso ./omp2012-1.1.iso /tmp/mnt_$BM
-	cd /tmp/mnt_$BM/
 	mkdir -p $ROOTDIR/$BM/
-	if [[ "$1" = *"fuji"* ]] && ! [[ "`hostname -s`" = *"peach"* ]]; then
-		./install.sh -f -d $ROOTDIR/$BM/ -u linux-apm-arm64
+	if [ ! -f $ROOTDIR/dep/mnt_$BM/shrc ]; then
+		if [ ! -f $ROOTDIR/dep/omp2012-1.1.iso ]; then
+			echo -e "ERR: cannot find omp2012-1.1.iso under dep/; please fix it!"
+			exit 1
+		fi
+		cd $ROOTDIR/dep/
+		mkdir -p /tmp/mnt_$BM
+		fuseiso ./omp2012-1.1.iso /tmp/mnt_$BM
+		cd /tmp/mnt_$BM/
+		if [[ "$1" = *"fuji"* ]] && ! [[ "`hostname -s`" = *"peach"* ]]; then
+			./install.sh -f -d $ROOTDIR/$BM/ -u linux-apm-arm64
+		else
+			./install.sh -f -d $ROOTDIR/$BM/ -u linux-suse10-amd64
+		fi
+		cd -
+		fusermount -u /tmp/mnt_$BM
 	else
-	        ./install.sh -f -d $ROOTDIR/$BM/ -u linux-suse10-amd64
+		cd $ROOTDIR/dep/mnt_$BM
+		if [[ "$1" = *"fuji"* ]] && ! [[ "`hostname -s`" = *"peach"* ]]; then
+			chmod -R +w $ROOTDIR/dep/mnt_$BM
+			for FILE in `find . -name config.guess`; do wget 'http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.guess' -O $FILE; done
+			FILE="tools/src/make-3.82/glob/glob.c"; sed -i -e 's@#if !defined __alloca && !defined __GNU_LIBRARY__@#if !defined __alloca@g' $FILE;
+			FILE="tools/src/perl-5.12.3/Configure"; sed -i -e 's@optimize="$ans"@optimize="-O1"@g' $FILE
+			FILE="tools/src/specsum/gnulib/stdio.in.h"; sed -i -e '/use fgets instead/i #if defined(__GLIBC__) && !defined(__UCLIBC__) && !__GLIBC_PREREQ(2, 16)' -e '/use fgets instead/a #endif' $FILE
+			FILE="tools/src/tar-1.25/gnu/stdio.in.h"; sed -i -e '/use fgets instead/i #if defined(__GLIBC__) && !defined(__UCLIBC__) && !__GLIBC_PREREQ(2, 16)' -e '/use fgets instead/a #endif' $FILE
+			#FILE="tools/src/buildtools"; sed -i -e '/IO-stringy\* \\/d' -e '/GD-\* \\/d' -e '/TimeDate-\* \\/d' $FILE
+			sed -i -e 's@MAKEFLAGS= $MYMAKE check; testordie "error testing tar"@#MAKEFLAGS= $MYMAKE check; testordie "error testing tar"@g' -e 's@MAKEFLAGS= $MYMAKE check; testordie "error testing specsum"@#MAKEFLAGS= $MYMAKE check; testordie "error testing specsum"@g' ./tools/src/buildtools
+			yes | ./tools/src/buildtools
+			bash -c "source ./shrc; runspec -V"
+			bash -c "source ./shrc; ./bin/packagetools fugaku"
+			bash -c "source ./shrc; specmd5sum ./fugaku-1.1.tar > ./fugaku-1.1.tar.md5"
+			#mkdir -p $ROOTDIR/$BM/bin/formatter; cp ./bin/flagutils.pl $ROOTDIR/$BM/bin/; cp ./bin/formatter/flagutils.pl $ROOTDIR/$BM/bin/formatter/
+			#cpan -I install Date::Parse
+			SPEC_INSTALL_NOCHECK=1 ./install.sh -f -d $ROOTDIR/$BM/ -u fugaku
+		else
+			./install.sh -f -d $ROOTDIR/$BM/ -u linux-suse10-amd64
+		fi
+		cd -
 	fi
-        cd -
-        fusermount -u /tmp/mnt_$BM
         cd $ROOTDIR/$BM/
         #patch -p1 < $ROOTDIR/patches/*1-${BM}*.patch
         dump_omp_config $ROOTDIR/$BM/config/nedo.cfg
@@ -171,10 +204,10 @@ if [ ! -f $ROOTDIR/$BM/bin/runcpu ]; then
 		bash -c "source ./shrc; runspec --config=nedo.cfg --action=build --size=train --define COMP=gnu --define RESDIR=0 gross"
 	elif [[ "$1" = *"fuji"* ]]; then
 		bash -c "source ./shrc; runspec --config=nedo.cfg --action=scrub --define COMP=fuji --define RESDIR=0 gross"
-		if [[ "`hostname -s`" = *"peach"* ]]; then
-			bash -c "source ./shrc; runspec --config=nedo.cfg --action=build --size=train --define COMP=fuji --define RESDIR=0 gross ^bt331 ^swim"	#XXX: peach fccpx doesnt support mcmodel
+		if [[ "`hostname -s`" = *"peach"* ]]; then    #XXX: peach fccpx/static doesnt support mcmodel
+			bash -c "source ./shrc; runspec --config=nedo.cfg --action=build --size=train --define COMP=fuji --define HOST=`hostname -s` --define RESDIR=0 gross ^bt331 ^swim"
 		else
-			bash -c "source ./shrc; runspec --config=nedo.cfg --action=build --size=train --define COMP=fuji --define RESDIR=0 gross"
+			bash -c "source ./shrc; runspec --config=nedo.cfg --action=build --size=train --define COMP=fuji --define HOST=`hostname -s` --define RESDIR=0 gross"
 		fi
 	fi
 	# check that most/all are static
