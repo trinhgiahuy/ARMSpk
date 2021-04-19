13c6a7751dc3e426b28802f1b9270a5424420e00
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-04-19 18:12:14 +0900

    proper patching

--- inst/fs2020.sh
+++ inst/fs2020.sh
@@ -1,32 +1,5 @@
 #!/bin/bash
 
-function prep_dir {
-	for FILE in `/bin/grep '__builtin_fj_prefetch' -r $1 | cut -d':' -f1 | sort -u`; do sed -i -e 's#__builtin_fj_prefetch#//__builtin_fj_prefetch#g' $FILE; done
-	for FILE in `/bin/grep 'PROF_START_ALL\|PROF_STOP_ALL' -r $1 | /bin/grep -v 'define' | cut -d':' -f1 | sort -u`; do
-		if [[ "$FILE" = *".c" ]] || [[ "$FILE" = *".cc" ]]; then
-			sed -i -e '/#include.*"profiler.h"/a #include <time.h>\n#include <stdio.h>' -e 's/PROF_INIT/double mkrts, mkrte; struct timespec mkrtsclock;/g' -e 's/PROF_START_ALL/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrts = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/g' -e 's/PROF_STOP_ALL/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrte = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/g' -e 's/PROF_FINALIZE/fprintf(stdout,"Walltime of the main kernel: %.6lf sec\\n", mkrte - mkrts);/g' $FILE
-		else
-			if ! [[ "$FILE" = *"postK_nicam_divdamp_ijsplit_tune03.f90" ]]; then sed -i -n 'p; s/implicit none/real :: mkrts, mkrte/p' $FILE; fi
-			sed -i -n 'p; s/PROF_START_ALL/call cpu_time(mkrts)/p' $FILE
-			sed -i -n 'p; s/PROF_STOP_ALL/call cpu_time(mkrte)/p' $FILE
-			if [[ "$FILE" = *".F" ]] || [[ "$FILE" = *".f" ]]; then
-				sed -i -n 'p; s/PROF_FINALIZE/write(*,"(A,f11.6,A)") "Walltime of the main kernel: ", \n     +  mkrte-mkrts, " sec"/p' $FILE
-			else
-				sed -i -n 'p; s/PROF_FINALIZE/write(*,"(A,f11.6,A)") "Walltime of the main kernel: ", \&\n     \&  mkrte-mkrts, " sec"/p' $FILE
-			fi
-			if [[ "$FILE" = *"kernel_pairlist_july.f90" ]] || [[ "$FILE" = *"kernel_july.f90" ]]; then
-				sed -i -n 'p; s/integer(1),.*pointer,contiguous :: exclusion_mask.*/real :: mkrts, mkrte/p' $FILE
-			fi
-			if [[ "$FILE" = *"postK_nicam_divdamp_ijsplit_tune03.f90" ]]; then
-				sed -i -n 'p; s/integer :: i$/real :: mkrts, mkrte/p' $FILE
-			fi
-		fi
-	done
-	if [ -z $2 ] && [[ "$1" = *"18.Adventure.region1.tune01.outputcheck.160808.M24"* ]]; then
-		for FILE in `/bin/grep 'implicit none' -r $1 | cut -d':' -f1 | sort -u`; do sed -i '0,/.*implicit none.*/ s///' $FILE; done
-	fi
-}
-
 function do_dir {
 	local OBJFILE=""
 	echo "==================== $1 ===================="
@@ -91,6 +64,7 @@ if [ ! -f $ROOTDIR/$BM/14.streamlike_pattern1/main ]; then
 	if ! [ -f $ROOTDIR/$BM/option.list ]; then
 		unzip -d $ROOTDIR/$BM/ $ROOTDIR/dep/fs2020_microkernel_tmp.zip && f=($ROOTDIR/$BM/*) && mv $ROOTDIR/$BM/*/* $ROOTDIR/$BM/ && rmdir "${f[@]}"
 	fi
+	patch -p1 < $ROOTDIR/patches/*1-${BM}*.patch
 	cat <<EOF > Makefile.inc
 DEF00=
 DEF01=
@@ -134,25 +108,27 @@ OBJFILE=
 	\$(FC) -c \$(FFLAGS) $< -o \$@
 EOF
 	if [ -z $1 ]; then
-		sed -i -e 's/CC=.*/CC=icc/g' -e 's/CXX=.*/CXX=icpc/g' -e 's/FC=.*/FC=ifort/g' -e 's/-Kopenmp -Bstatic/-qopenmp -static -static-intel/g' Makefile.inc
-		sed -i -e 's/$(.OPTIMIZE) -Bstatic/$(DEF00) -fpp -std=gnu99 -qopenmp -O3 -xHost -static -static-intel/g' Makefile.inc
+		sed -i -e 's/CC=.*/CC=icc/g' -e 's/CXX=.*/CXX=icpc/g' -e 's/FC=.*/FC=ifort/g' -e 's#-Kopenmp -Bstatic#-qopenmp -static -static-intel -L${ADVISOR_2018_DIR}/lib64 -littnotify#g' Makefile.inc
+		sed -i -e 's#$(.OPTIMIZE) -Bstatic#$(DEF00) -fpp -std=gnu99 -qopenmp -O3 -xHost -static -static-intel -I${ADVISOR_2018_DIR}/include#g' Makefile.inc
 	elif [[ "$1" = *"gnu"* ]]; then
 		sed -i -e 's/CC=.*/CC=gcc/g' -e 's/CXX=.*/CXX=g++/g' -e 's/FC=.*/FC=gfortran/g' -e 's/-Kopenmp -Bstatic/-fopenmp -static/g' Makefile.inc
 		sed -i -e 's/$(.OPTIMIZE) -Bstatic/$(DEF00) -fopenmp -O3 -march=native -static/g' Makefile.inc
+		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' $FILE; done
 	elif lscpu | grep 'sve' >/dev/null 2>&1 && [[ "$1" = *"fuji"* ]]; then
 		sed -i -e 's/-Bstatic//g' Makefile.inc
+		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify.h.*/#include "fj_tool/fapp.h"\n#define __itt_resume() fapp_start("kernel",1,0);\n#define __itt_pause() fapp_stop("kernel",1,0);\n#define __SSC_MARK(hex)/' $FILE; done
 	elif [[ "`hostname -s`" = *"fn01"* ]] && [[ "$1" = *"fuji"* ]]; then
 		sed -i -e 's/-Bstatic//g' Makefile.inc
+		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify.h.*/#include "fj_tool/fapp.h"\n#define __itt_resume() fapp_start("kernel",1,0);\n#define __itt_pause() fapp_stop("kernel",1,0);\n#define __SSC_MARK(hex)/' $FILE; done
 	elif [[ "$1" = *"arm"* ]]; then
 		sed -i -e 's/CC=.*/CC=armclang/g' -e 's/CXX=.*/CXX=armclang++/g' -e 's/FC=.*/FC=armflang/g' -e 's/-Kopenmp -Bstatic/-fopenmp/g' Makefile.inc
 		sed -i -e 's/$(.OPTIMIZE) -Bstatic/$(DEF00) -fopenmp -mcpu=a64fx -march=armv8.2-a+sve -Ofast -ffast-math -flto/g' Makefile.inc
+		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' $FILE; done
 	fi
 	for BMconf in ${BINARYS}; do
 		NR=`echo $BMconf | cut -d'.' -f1`; sed -i -e "s/(DEF..)/(DEF$NR)/g" Makefile.inc
-		prep_dir $ROOTDIR/$BM/$(dirname $BMconf) $1
 		do_dir $ROOTDIR/$BM/$(dirname $BMconf)
 	done
 	find $ROOTDIR/$BM/ -executable -type f -exec echo {} \; -exec ldd {} \;
 	cd $ROOTDIR
 fi
-
