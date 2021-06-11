d2f6e3de88ddb7bf5e30f014dfc1a45c15ad5f98
Jens Domke <domke.j.aa@m.titech.ac.jp>
2021-06-11 22:34:40 +0900

    clangify for fu

--- inst/hpl.sh
+++ inst/hpl.sh
@@ -31,7 +31,9 @@ elif [[ "$1" = *"gnu"* ]]; then
 	export OMPI_F77=gfortran
 	export OMPI_FC=gfortran
 elif [[ "$1" = *"fuji"* ]]; then
-	module load FujitsuCompiler/201903
+	sleep 0
+elif [[ "$1" = *"gem5"* ]]; then
+	#module load FujitsuCompiler/201903
 	#export PATH=$ROOTDIR/dep/mpistub/bin:$PATH	#XXX: those won't run because of missing cross-compile
 	export LD_LIBRARY_PATH=$ROOTDIR/dep/mpistub/lib:$LD_LIBRARY_PATH
 fi
@@ -40,7 +42,7 @@ BM="HPL"
 if [ ! -f $ROOTDIR/$BM/bin/Linux_Intel64/xhpl ]; then
 	mkdir -p $ROOTDIR/$BM/
 	cd $ROOTDIR/$BM/
-	wget http://www.netlib.org/benchmark/hpl/hpl-2.2.tar.gz
+	if [ ! -f ./hpl-2.2.tar.gz ]; then wget http://www.netlib.org/benchmark/hpl/hpl-2.2.tar.gz; fi
 	tar xzf ./hpl-2.2.tar.gz -C $ROOTDIR/$BM --strip-components 1
 	patch -p1 --forward < $ROOTDIR/patches/*1-${BM}*.patch
 	if [ -z $1 ]; then
@@ -51,10 +53,8 @@ if [ ! -f $ROOTDIR/$BM/bin/Linux_Intel64/xhpl ]; then
 	elif [[ "`hostname -s`" = *"fn01"* ]] && [[ "$1" = *"fuji"* ]]; then
 		sed -i -e 's/mpiicc/mpifccpx/' -e 's/-ipo -xHost/-Kfast/g' -e 's/ -z noexecstack -z relro -z now//g' -e 's/ -Wall//g' -e "s# -I\${ADVISOR_2018_DIR}/include# -I$ROOTDIR/dep/mpistub/include/mpistub#g" -e "s# -L\${ADVISOR_2018_DIR}/lib64 -littnotify##g" -e "s#\$(HOME)/hpl#$ROOTDIR/$BM#g" -e 's/ -ansi-alias -i-static//g' -e 's/ -nocompchk//g' -e 's/ -mt_mpi//g' -e 's/ -I$(LAinc)//g' -e "s# \$(LAlib)# -SSL2BLAMP#g" ./Make.Linux_Intel64
 		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify.h.*/#include "fj_tool\/fapp.h"\n#define __itt_resume() fapp_start("kernel",1,0);\n#define __itt_pause() fapp_stop("kernel",1,0);\n#define __SSC_MARK(hex)/' $FILE; done
-	elif [[ "$1" = *"fuji"* ]]; then
-		SSL2LIB=/opt/FJT/FJTMathlibs_201903/lib64			# new Fj version lacks ssl2
-		ln -s $(dirname `which fccpx`)/../lib64/libfj90rt2.a $ROOTDIR/$BM/libfj90rt.a	# fix broken linker
-		sed -i -e 's/mpiicc/fccpx/' -e 's/-ipo -xHost//g' -e 's/ -z noexecstack -z relro -z now//g' -e 's/ -Wall//g' -e "s# -I\${ADVISOR_2018_DIR}/include# -I$ROOTDIR/dep/mpistub/include/mpistub#g" -e "s# -L\${ADVISOR_2018_DIR}/lib64 -littnotify# -Wl,-rpath -Wl,$ROOTDIR/dep/mpistub/lib/mpistub -L$ROOTDIR/dep/mpistub/lib/mpistub -lmpi -Bstatic#g" -e "s#\$(HOME)/hpl#$ROOTDIR/$BM#g" -e 's/ -ansi-alias -i-static//g' -e 's/ -nocompchk//g' -e 's/ -mt_mpi//g' -e 's/ -I$(LAinc)//g' -e "s# \$(LAlib)# -L$ROOTDIR/$BM/ -L$SSL2LIB -SSL2BLAMP#g" ./Make.Linux_Intel64
+	elif [[ "$1" = *"gem5"* ]]; then
+		sed -i -e 's/mpiicc/fccpx/' -e 's/-ipo -xHost/-Kfast -Knolargepage/g' -e 's/ -z noexecstack -z relro -z now//g' -e 's/ -Wall//g' -e "s# -I\${ADVISOR_2018_DIR}/include# -I$ROOTDIR/dep/mpistub/include/mpistub#g" -e "s# -L\${ADVISOR_2018_DIR}/lib64 -littnotify# -Wl,-rpath -Wl,$ROOTDIR/dep/mpistub/lib/mpistub -L$ROOTDIR/dep/mpistub/lib/mpistub -lmpi#g" -e "s#\$(HOME)/hpl#$ROOTDIR/$BM#g" -e 's/ -ansi-alias -i-static//g' -e 's/ -nocompchk//g' -e 's/ -mt_mpi//g' -e 's/ -I$(LAinc)//g' -e "s# \$(LAlib)# -L$ROOTDIR/$BM/ -SSL2BLAMP#g" ./Make.Linux_Intel64
 		for FILE in `/usr/bin/grep 'include.*ittnotify' -r | cut -d':' -f1 | sort -u`; do sed -i -e 's/.*include.*ittnotify\.h.*/#include <time.h>\n#define __itt_resume()\n#define __itt_pause()\n#define __SSC_MARK(hex)/' -e '/double mkrts, mkrte;/i struct timespec mkrtsclock;' -e 's/mkrts = MPI_Wtime();/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrts = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/' -e 's/mkrte = MPI_Wtime();/clock_gettime(CLOCK_MONOTONIC, \&mkrtsclock); mkrte = (mkrtsclock.tv_sec + mkrtsclock.tv_nsec * .000000001);/' $FILE; done
 	fi
 	make arch=Linux_Intel64
