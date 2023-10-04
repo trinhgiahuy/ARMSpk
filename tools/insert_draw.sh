2dbf9946f0f5b0c4587a3410ba1afd4f00d66ef3
Huy Trinh <huy.trinh@supercomp07a.r-ccs27.riken.jp>
2023-10-04 17:59:20 +0900

    For NICAM, it will add the linking directory check

--- tools/insert_draw.sh
+++ tools/insert_draw.sh
@@ -7,6 +7,8 @@
 #       The script will add checl log file code to run file. In some cases,
 #       run file cannot automatically create new log file
 #
+#       For NICAM benchmark, it will conditional check for existing directory of
+#       data symbolic link
 # Usage:
 #
 # 	    insert_draw.sh $ROOTDIR/run/amg/test.sh
@@ -30,8 +32,8 @@ INPUT_FILE=$1
 
 CHECK_LOG_CODE="\
 if [ ! -e \$LOG ];then
-    echo \"[\$0] DOES NOT HAVE LOG FILE. CREATING...\"
-    touch \$LOG
+        echo \"[\$0] DOES NOT HAVE LOG FILE. CREATING...\"
+        touch \$LOG
 fi
 
 "
@@ -39,27 +41,77 @@ fi
 
 
 if ! grep -q "COUNTER" "$INPUT_FILE"; then
+    last_test_conf_line=$(grep -nE 'TESTCONF' "$INPUT_FILE" | tail -n1 | cut -d: -f1)
+    echo last_test_conf_line $last_test_conf_line
 
-  # WORKING_DIR="$(cd $(dirname $1)/../../ && pwd)"
-  # base_name="${INPUT_FILE%.sh}"
-  # backup_file="$WORKING_DIR/${base_name}_bku.sh"
-
-  # cp -p "$INPUT_FILE" "$backup_file"
-  original_permissions=$(stat -c %a "$INPUT_FILE")
-  TEMP_FILE=$(mktemp)
-  awk -v check_log_code="${CHECK_LOG_CODE}" '
-  {
-      if ($0 ~ /\$\{TESTCONF\}/) {
-          print check_log_code
-          print "TOTAL_TESTS=$(echo $TESTCONF | tr \" \" \"\\n\" | wc -l)\necho \"TOTAL_TESTS: $TOTAL_TESTS\"\nCOUNTER=0"
-          print $0
-          print "\tCOUNTER=$((COUNTER+1))\n\tdraw_progress_bar $COUNTER $TOTAL_TESTS"
-      } else {
-          print $0
-      }
-  }' "${INPUT_FILE}" > "${TEMP_FILE}"
-  mv "${TEMP_FILE}" "${INPUT_FILE}"
-  chmod "$original_permissions" "$INPUT_FILE"
+    original_permissions=$(stat -c %a "$INPUT_FILE")
+    TEMP_FILE=$(mktemp)
+    awk -v check_log_code="${CHECK_LOG_CODE}" -v line_num="${last_test_conf_line}" '
+    {
+        if ( NR == line_num){
+            print check_log_code
+            print "TOTAL_TESTS=$(echo $TESTCONF | tr \" \" \"\\n\" | wc -l)\necho \"TOTAL_TESTS: $TOTAL_TESTS\"\nCOUNTER=0"
+            print $0
+            print "\tCOUNTER=$((COUNTER+1))\n\tdraw_progress_bar $COUNTER $TOTAL_TESTS"
+        } else {
+            print $0
+        }
+    }' "${INPUT_FILE}" > "${TEMP_FILE}"
+    mv "${TEMP_FILE}" "${INPUT_FILE}"
+    chmod "$original_permissions" "$INPUT_FILE"
 else
-  echo "The file $1 already contain progress bar!"
+    echo "The file $1 already contain progress bar!"
 fi
+
+
+LINK_SYSDEP_CODE="\
+    cd ../../../sysdep
+    [ ! -e Makedef.ARM64 ] && ln -s ../../sysdep/Makedef.ARM64 .
+    cd -"
+
+# Add link sysdep to files test of NICAM
+bm_id=$(echo $INPUT_FILE | cut -d/ -f2 )
+echo bm_id $bm_id
+
+if [[ $bm_id =~ nicam ]];then
+    echo "Got nicam"
+    make_jobshell_line=$(grep -nE 'jobshell' "$INPUT_FILE" | cut -d: -f1)
+    echo "jobshell_line $make_jobshell_line"
+
+    original_permissions=$(stat -c %a "$INPUT_FILE")
+    TEMP_FILE=$(mktemp)
+    if ! grep -q "sysdep" "$INPUT_FILE";then
+        echo "Adding link sysdep"
+        awk -v link_sysdep_code="$LINK_SYSDEP_CODE" -v line_num="${make_jobshell_line}" '
+        {
+            if ( NR == line_num){
+                print $0
+            }else{
+                print $0
+            }
+        }' "${INPUT_FILE}" > "${TEMP_FILE}"
+        mv "${TEMP_FILE}" "${INPUT_FILE}"
+        chmod "$original_permissions" "${INPUT_FILE}"
+    else
+        echo "Already has link code"
+    fi
+    # SHOULD BE
+    # Makedef.ARM64 nhm_driver rl00-prc10.info vgrid40_24000-600m.dat boundary_GL05RL00.pe000000 boundary_GL05RL00.pe000001
+    # boundary_GL05RL00.pe000002 boundary_GL05RL00.pe000003 boundary_GL05RL00.pe000004 boundary_GL05RL00.pe000005
+    # boundary_GL05RL00.pe000006 boundary_GL05RL00.pe000007 boundary_GL05RL00.pe000008 boundary_GL05RL00.pe000009
+    linking_files_name_arr=($(grep -n 'ln' run/nicam/test.sh | rev |  cut -d/ -f1| cut -d' ' -f2 | rev))
+    echo "${linking_files_name_arr[@]}"
+
+    # TEMP_FILE=$(mktemp)
+    # cp -p $INPUT_FILE $TEMP_FILE
+    for fn in "${linking_files_name_arr[@]}";do
+        echo $fn
+        # sed -i '/ln -s .* '"$fn"'$/s#^#[ ! -e '"$fn"' ] \&\& #' $TEMP_FILE
+        sed -i '/ln -s .*'"$fn"'.*/s#^\t#\t[ ! -e '"./$fn"' ] \&\& #' $INPUT_FILE
+    done
+
+    # mv $TEMP_FILE $INPUT_FILE
+fi
+
+
+
